28dbe8691cc027b34989f5774b5a23144f2eda81
John Foley <jfoley@pivotal.io>
2016-12-21 10:08:47 -0700

    add redis to store session

--- readme.md
+++ readme.md
@@ -7,12 +7,21 @@ hits the same instance they logged in to.
 
 We can demonstrate this by running two instances of our server, and then running a load balancer in front of them:
 in one terminal window run:
-`-Dmaven.tomcat.port=8080 -DinstanceNumber=1 tomcat7:run-war`
+`mvn -Dmaven.tomcat.port=8080 -DinstanceNumber=1 tomcat7:run-war`
 
 and in another terminal window run:
-`-Dmaven.tomcat.port=9090 -DinstanceNumber=2 tomcat7:run-war`
+`mvn -Dmaven.tomcat.port=9090 -DinstanceNumber=2 tomcat7:run-war`
 
 and in a third window run:
 `./start-load-balancer.sh`
 
 Take this example, and introduce a redis server to hold the state.
+
+## Step 2
+
+Now that we have moved our session state in to redis, we can start to take
+advantage of the tools that cloud foundry provides- rather than running our
+ own instance of redis, we can use the one provided by the platform.
+ 
+ Introduce the spring cloud service connector, and use that to connect to 
+ the platform provided redis instance.
---
+++ src/main/java/io/pivotal/pal/SpringInitializer.java
@@ -0,0 +1,24 @@
+package io.pivotal.pal;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.filter.DelegatingFilterProxy;
+
+import javax.servlet.ServletContainerInitializer;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import java.util.Set;
+
+public class SpringInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setInitParameter("contextClass", "org.springframework.web.context.support.AnnotationConfigWebApplicationContext");
+        servletContext.setInitParameter("contextConfigLocation", "io.pivotal.pal");
+        servletContext.addListener(ContextLoaderListener.class);
+
+        servletContext.addFilter("springSessionRepositoryFilter", DelegatingFilterProxy.class)
+                .addMappingForUrlPatterns(null, false, "/*");
+    }
+}
+
+
